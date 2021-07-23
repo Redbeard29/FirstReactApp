ad5d06249fb0b2e92cbb1e1e2fdfb22f8c8dc219
Redbeard29 <ben@benshermanmusic.com>
2021-07-23 11:17:37 -0400

    added a component to filter expenses by year

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,21 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+let x = 5;
+
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: 'e' + (x.toString())
+        };
+        x++;
+        props.onAddExpense(expenseData);
+    };
+
     return <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
 };
 
